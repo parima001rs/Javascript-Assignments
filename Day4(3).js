64f176fad28dc880ab4a3e113385165296a764b8
Parima Kashyap <65702724+parima001rs@users.noreply.github.com>
2020-07-18 13:10:45 +0530

    Create Day4(3).js

---
+++ Day4(3).js
@@ -0,0 +1,4 @@
+let shoppingList = ['Product1','Product2','Product3'];   
+let shoppingBasket = ['...shoppingList,'Product4','Product5'];  
+shoppingBasket.push('Product6'); 
+console.log(shoppingBasket);
