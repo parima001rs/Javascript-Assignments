6f9c6e8201628ad5e4444e872a05bcdb10b25403
Parima Kashyap <65702724+parima001rs@users.noreply.github.com>
2020-07-18 16:11:03 +0530

    Update Day4(4).js

--- Day4(4).js
+++ Day4(4).js
@@ -1,1 +1,22 @@
 //Calculator 
+ alert("Following are operators:  + - * / % sqrt ")
+        let arr = prompt("Enter an expression with two operands and one operator seperated by spaces.Example:5 + 5").trim().split(" ");
+        switch(arr[1]){
+            case '+':
+            console.log(Number(arr[0])+Number(arr[2]));
+            break;
+            case '-':
+            console.log(Number(arr[0])-Number(arr[2]));
+            break;
+            case '*':
+            console.log(Number(arr[0])*Number(arr[2]));
+            break;
+            case '/':
+            console.log(Number(arr[0])/Number(arr[2]));
+            break;
+            case 'sqrt':
+            console.log(Math.sqrt(Number(arr[0]),Number(arr[2])));
+            break;
+            case '%':
+            console.log(Number(arr[0]) / Number(arr[2]) * 100);
+            break;
