67cc7e720cb60a803842e78226309186903e09b9
Parima Kashyap <65702724+parima001rs@users.noreply.github.com>
2020-07-18 16:36:42 +0530

    Update Day4(2).js

--- Day4(2).js
+++ Day4(2).js
@@ -1,1 +1,10 @@
-const {name,age,projects:{diceGame}} = student;
+const student = { 
+            name: "Helsinki",
+            age: 24,
+            project: {
+                diceGame: "Two player dice game using JavaScript",
+            },
+        }
+const {name,age,project:{diceGame}} = student;
+console.log(name, age, project);
+
