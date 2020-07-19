7cb965e0fdd536d848fc00d502afadfbd7ee026e
Parima Kashyap <65702724+parima001rs@users.noreply.github.com>
2020-07-19 20:20:08 +0530

    Update Day4(5).js

--- Day4(5).js
+++ Day4(5).js
@@ -1,15 +1,17 @@
-function saleReward(sales){
-if(sales<=5000){
-reward = sales * 0.02;
-}
-if(sales>=5001 && sales<=10000){
-reward = sales * 0.05;
-}
-if(sales>=10001 && sales<=20000){
-reward = sales * 0.07;
-}
-if(sales>5000){
-reward = sales * 0.10;
-}
-
-saleReward(prompt(sales));
+let sales = prompt(`enter the sales`);
+        if(sales <= 5000){
+            reward = sales*0.02;
+            console.log(reward);
+        }
+        if(sales>=5001 && sales<=10000){
+            reward = sales*0.05;
+            console.log(reward);
+        }
+        if(sales>=10001 && sales<=20000){
+            reward = sales * 0.07;
+            console.log(reward);
+            }
+        if(sales>5000){
+            reward = sales * 0.10;
+            console.log(reward);
+            }
